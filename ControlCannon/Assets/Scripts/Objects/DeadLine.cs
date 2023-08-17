73c275ffdb8c22e20921212e855ba35b6b248650
Junghyeon <tlslrs1994@gmail.com>
2023-08-17 22:44:10 +0900

    feat : 데드라인 구현
    - 적 몹들을 터치했을 때 스테이지를 종료시킬 데드라인 구현

---
+++ ControlCannon/Assets/Scripts/Objects/DeadLine.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(Define.TAG_ENEMY_MOB))
+            other.gameObject.SetActive(false);
+    }
+}
