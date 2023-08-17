cf890c01e37994933e5fe45f5ae95dc508d18477
Junghyeon <tlslrs1994@gmail.com>
2023-08-17 17:51:01 +0900

    fix : 씬을 종료할 때 문제 수정
    - 씬을 종료해도 싱글톤 인스턴스를 다시 생성돼 오브젝트가 생성되는 문제 수정

--- ControlCannon/Assets/Scripts/Managers/Manager.cs
+++ ControlCannon/Assets/Scripts/Managers/Manager.cs
@@ -4,34 +4,33 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    private static Manager _instance;
-
-    public static Manager Instance
-    {
-        get
-        {
-            if (null == _instance)
-                _instance = GameObject.FindObjectOfType<Manager>();
-            if (null == _instance)
-            {
-                var go = new GameObject(MANAGER);
-                _instance = go.AddComponent<Manager>();
-            }
-            return _instance;
-        }
-    }
-
     private ObjectManager _object;
 
     public ObjectManager Object { get { return _object; } }
 
-    private const string MANAGER = "[MANAGER]";
-
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
+        CreateInstance();
 
         _object = new ObjectManager();
         _object.Init();
     }
+
+    #region Instance
+    private static Manager _instance;
+
+    public static Manager Instance { get { return _instance; } }
+
+    public void CreateInstance()
+    {
+        if (null == _instance)
+            _instance = GameObject.FindObjectOfType<Manager>();
+        if (null == _instance)
+        {
+            var go = new GameObject(Define.MANAGER);
+            _instance = go.AddComponent<Manager>();
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+    #endregion
 }
--- ControlCannon/Assets/Scripts/Utils/Define.cs
+++ ControlCannon/Assets/Scripts/Utils/Define.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Define
 {
+    //Object Name
+    public const string MANAGER = "[MANAGER]";
+
     //Tag
     public const string TAG_MOB = "Mob";
     public const string TAG_ENEMY_MOB = "EnemyMob";
