8f22ad3ac1c910677236c2f0aa97fc8cf01031dd
Junghyeon <tlslrs1994@gmail.com>
2023-08-21 12:47:37 +0900

    fix : 승리, 패배시 오브젝트 수거 수정
    - 모든 오브젝트들 수거시 해당 타입에 맞는 오브젝트풀에 수거하게끔 수정

--- ControlCannon/Assets/Scripts/Managers/ObjectManager.cs
+++ ControlCannon/Assets/Scripts/Managers/ObjectManager.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public enum EObjectTypes
 {
-    Mob,
+    AllyMob,
     EnemyMob,
     Gate,
 
@@ -14,12 +15,17 @@ public enum EObjectTypes
 
 public class ObjectManager
 {
-    private GameObject _cannon;
+    private struct UsedObjects
+    {
+        public EObjectTypes ObjectsType { get; set; }
+        public GameObject UsedObject { get; set; }
+    }
+
     private ICannonController _currentCannon;
     private GameObject _castle;
     private Dictionary<EObjectTypes, ObjectPool> _objectsDic;
 
-    private Queue<GameObject> _usedObjectsQueue;
+    private Queue<UsedObjects> _usedObjectsQueue;
 
     public ICannonController CurrentCannon { get { return _currentCannon; } }
     public GameObject Castle { get { return _castle; } }
@@ -30,8 +36,8 @@ public class ObjectManager
     public void Init()
     {
         var cannonParent = new GameObject(Define.ROOT_CANNON);
-        _cannon = Manager.Instance.Resource.Instantiate(Define.RESOURCE_OBJECT_CANNON, cannonParent);
-        _currentCannon = _cannon.GetComponent<CannonController>();
+        var cannon = Manager.Instance.Resource.Instantiate(Define.RESOURCE_OBJECT_CANNON, cannonParent);
+        _currentCannon = cannon.GetComponent<CannonController>();
 
         var castleParent = new GameObject(Define.ROOT_CASTLE);
         _castle = Manager.Instance.Resource.Instantiate(Define.RESOURCE_OBJECT_CASTLE, castleParent);
@@ -40,21 +46,21 @@ public class ObjectManager
         _objectsDic = new Dictionary<EObjectTypes, ObjectPool>();
         for (EObjectTypes ii = 0; ii < EObjectTypes.COUNT; ++ii)
             _objectsDic.Add(ii, new ObjectPool());
-
         var objectsParent = new GameObject(Define.OBJECT_POOL);
         var mob = Manager.Instance.Resource.Load<GameObject>(Define.RESOURCE_OBJECT_MOB);
-        _objectsDic[EObjectTypes.Mob].InitPool(mob, objectsParent, CREATE_MOB_COUNT);
+        _objectsDic[EObjectTypes.AllyMob].InitPool(mob, objectsParent, CREATE_MOB_COUNT);
         _objectsDic[EObjectTypes.EnemyMob].InitPool(mob, objectsParent, CREATE_MOB_COUNT);
         var gate = Manager.Instance.Resource.Load<GameObject>(Define.RESOURCE_OBJECT_GATE);
         _objectsDic[EObjectTypes.Gate].InitPool(gate, objectsParent, CREATE_GATE_COUNT);
 
-        _usedObjectsQueue = new Queue<GameObject>();
+        _usedObjectsQueue = new Queue<UsedObjects>();
     }
 
     public GameObject GetObject(EObjectTypes type)
     {
         var obj = _objectsDic[type].GetObject();
-        _usedObjectsQueue.Enqueue(obj);
+        var usedObject = new UsedObjects() { ObjectsType = type, UsedObject = obj };
+        _usedObjectsQueue.Enqueue(usedObject);
         return obj;
     }
 
@@ -63,8 +69,11 @@ public class ObjectManager
         while (_usedObjectsQueue.Count > 0)
         {
             var usedObject = _usedObjectsQueue.Dequeue();
-            if (usedObject.activeSelf)
-                usedObject.SetActive(false);
+            if (usedObject.UsedObject.activeSelf)
+            {
+                usedObject.UsedObject.SetActive(false);
+                ReturnObject(usedObject.ObjectsType, usedObject.UsedObject);
+            }
         }
     }
 
